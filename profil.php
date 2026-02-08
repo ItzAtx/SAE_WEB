e5923dd338cddd24d2b3c263a9c86bbd12baab85
ItzAtx <anthony.vauchel.cours@gmail.com>
2026-02-08 14:07:47 +0100

    Ajout du css pour la page de profil, du changement de mot de passe et du bouton de connexion

--- profil.php
+++ profil.php
@@ -7,50 +7,75 @@
 ?>
 
 <html>
+    <head>
+        <link rel="stylesheet" href="css/profil.css">
+    </head>
     <body>
-        <?php
-            include("connex.inc.php");
-            $idco = connex("myparam", "zoo");
+        <div class="container">
+            <div class="left">
+                <h2>Informations</h2>
+                <?php
+                    include("connex.inc.php");
+                    $idco = connex("myparam", "zoo");
 
-            $id = $_SESSION['id'];
-            $requete = "SELECT * FROM Personnel WHERE numero_personnel = $id";
-            $result = mysqli_query($idco, $requete);
-            $row = mysqli_fetch_array($result);
+                    $id = $_SESSION['id'];
+                    $requete = "SELECT * FROM Personnel WHERE numero_personnel = $id";
+                    $result = mysqli_query($idco, $requete);
+                    $row = mysqli_fetch_array($result);
 
-            $num = $row['numero_personnel'];
-            $prenom = $row['prenom_personnel'];
-            $nom = $row['nom_personnel'];
-            $date = $row['date_entree_personnel'];
-            $salaire = $row['salaire_personnel'];
-            $identifiant = $row['identifiant_personnel'];
-            $id_fonction = $row['id_fonction'];
+                    $num = $row['numero_personnel'];
+                    $prenom = $row['prenom_personnel'];
+                    $nom = $row['nom_personnel'];
+                    $date = $row['date_entree_personnel'];
+                    $salaire = $row['salaire_personnel'];
+                    $identifiant = $row['identifiant_personnel'];
+                    $id_fonction = $row['id_fonction'];
 
-            $requete = "SELECT nom_fonction FROM Fonction WHERE id_fonction = $id_fonction";
-            $result = mysqli_query($idco, $requete);
-            $row = mysqli_fetch_array($result);
-            $fonction = $row['nom_fonction'];
+                    $requete = "SELECT nom_fonction FROM Fonction WHERE id_fonction = $id_fonction";
+                    $result = mysqli_query($idco, $requete);
+                    $row = mysqli_fetch_array($result);
+                    $fonction = $row['nom_fonction'];
+                ?>
 
-            echo "Numéro de personnel : $num <br/>";
-            echo "Prénom : $prenom <br/>";
-            echo "Nom : $nom <br/>";
-            echo "Date d'entrée : $date <br/>";
-            echo "Salaire mensuel : $salaire <br/>";
-            echo "Identifiant de connexion : $identifiant <br/>";
-            echo "Poste : $fonction <br/>";
-        ?>
+                <div class="info">Numéro de personnel : <?php echo $num; ?></div>
+                <div class="info">Prénom : <?php echo $prenom; ?></div>
+                <div class="info">Nom : <?php echo $nom; ?></div>
+                <div class="info">Date d'entrée : <?php echo $date; ?></div>
+                <div class="info">Salaire mensuel : <?php echo $salaire; ?></div>
+                <div class="info">Identifiant de connexion : <?php echo $identifiant; ?></div>
+                <div class="info">Poste : <?php echo $fonction; ?></div>
+            </div>
 
-        <form method="post">
-            Nouveau mot de passe : <input type="text" name="nvmdp">
-            <input type="submit" value="Confirmer">
-        </form>
+            <div class="separator"></div>
 
-        <?php
-            if (isset($_POST['nvmdp'])){
-                $nouveau = md5($_POST['nvmdp']);
+            <div class="right">
+                <h2>Sécurité</h2>
 
-                $requete = "UPDATE Personnel SET mdp_personnel = '$nouveau' WHERE numero_personnel = $id";
-                mysqli_query($idco, $requete);
-            }
-        ?>
+                <form method="post">
+                    Nouveau mot de passe : <input type="text" name="nvmdp">
+                    <input type="submit" value="Confirmer">
+                </form>
+
+                <?php
+
+                    if (isset($_POST['nvmdp'])){
+                        if (!empty($_POST['nvmdp'])){
+                            $nouveau = md5($_POST['nvmdp']);
+
+                            $requete = "UPDATE Personnel SET mdp_personnel = '$nouveau' WHERE numero_personnel = $id";
+                            mysqli_query($idco, $requete);
+                            echo "Mot de passe modifié !";
+                        } else {
+                            echo "Veuillez saisir un nouveau mot de passe";
+                        }
+                    }
+                    
+
+                    
+                ?>
+
+                <a href="login.php"><button>Deconnexion</button></a>
+            </div>
+        </div>
     </body>
 </html>
